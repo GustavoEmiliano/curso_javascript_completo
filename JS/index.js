02f616516efebcaf8682b0722761929e7c5f7ae1
Gustavo Emiliano <gustavoemiliano2014@gmail.com>
2024-06-27 20:36:26 -0300

    Aula 02: Tipos de saída

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,3 @@
-alert('Um alerta rodando de uma página externa');
+document.getElementById("texto").innerHTML="Meu primeiro texto <b>JS</b>!";
+console.log('Oi isso é um console.log');
+alert('Isso é um alerta')
