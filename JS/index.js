df7b1d85af53b59ae3723f24d225379667e7acca
Gustavo Emiliano <gustavoemiliano2014@gmail.com>
2024-06-27 20:52:33 -0300

    Aula 03: Declarações

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,18 @@
-document.getElementById("texto").innerHTML="Meu primeiro texto <b>JS</b>!";
-console.log('Oi isso é um console.log');
-alert('Isso é um alerta')
+let a,b,c;
+a = 5;
+b = 6;
+c = 7;
+
+let pessoa = "Dimitri";
+let pessoa="Dimitri";
+
+document.getElementById("texto").innerHTML =
+"Meu primeiro texto <b>JS</b>!";
+
+function minhafuncao(){
+    document.write(5 + 5);
+    document.write(5 + 5);
+    document.write(5 + 5);
+    document.write(5 + 5);
+}
+
