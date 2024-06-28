33b1e88f86c17b56ef429f208cf4d2ad3b52a5ee
Gustavo Emiliano <gustavoemiliano2014@gmail.com>
2024-06-27 21:13:05 -0300

    Aula 05: Comentários

--- JS/index.js
+++ JS/index.js
@@ -1,25 +1,9 @@
-/*String - Number*/
+// Inserindo um texto no paragrafo
+document.getElementById("texto").innerHTML =
+    "Meu primeiro texto <b>Javascript</b>";
 
-console.log("5 + 5");
-console.log(5 + 5);
+/*
+Autor: Gustavo;
+Script feito em: 27/06/24
 
-/*Atribuição de valores*/
-
-var a = 1;
-var b = 5;
-var c = a + b;
-console.log(c);
-
-/*Case sensitive*/
-
-var PESSOA = "Gustavo";
-var pessoa = "Emiliano";
-
-console.log(pessoa);
-console.log(PESSOA);
-
-/*Ideal é a utilização do padrão camelCase ou a utilização de _*/
-
-var primeiroNome;
-var primeiro_nome;
-
+*/
